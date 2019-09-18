b8f914feac9c317ad90e1827aacd921047bc2c7f
FoxitaMV <leto.web@ya.ru>
2019-09-18 17:10:13 +0300

    var. 0.005

--- form/service.py
+++ form/service.py
@@ -4,16 +4,16 @@ import time
 import math
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver.get("https://demo.bmw.kodixauto.ru/offers-and-services/test-drive/") 
+driver.get("https://demo.bmw.kodixauto.ru/owners/vehicle-service-parts/service-form/") 
 time.sleep(2)
 
-# link1 = driver.find_element_by_xpath('//*[@id="vin"]')
-# link1.send_keys('12345678912345678')
-# time.sleep(1)
+link1 = driver.find_element_by_xpath('//*[@id="vin"]')
+link1.send_keys('12345678912345678')
+time.sleep(1)
 
-# link2 = driver.find_element_by_xpath('//*[@id="year"]')
-# link2.send_keys('2012')
-# time.sleep(1)
+link2 = driver.find_element_by_xpath('//*[@id="year"]')
+link2.send_keys('2012')
+time.sleep(1)
 
 link3 = driver.find_element_by_xpath('//*[@id="datepicker"]')
 link3.send_keys('20092019')
@@ -44,17 +44,16 @@ mail = driver.find_element_by_xpath('//*[@id="email"]')
 mail.send_keys('dmp@kodix.ru')
 time.sleep(1)
 
-# var1 = driver.find_element_by_xpath('//*[@id="replacement_car"]')
-# var1.click()
-# time.sleep(1)
+var1 = driver.find_element_by_xpath('//*[@id="replacement_car"]')
+var1.click()
+time.sleep(1)
 
 var2 = driver.find_element_by_xpath('//*[@id="processing_of_personal_data"]')
 var2.click()
 time.sleep(1)
 
-btn = driver.find_element_by_xpath('/html/body/div[1]/main/div/div/div/div/div/div/div/div/div/div[2]/form/div/div[5]/div/button')
+btn = driver.find_element_by_xpath('/html/body/div[1]/main/div/div/div/div/div/div/div/div/div/div[2]/form/div/div[6]/div/button')
 btn.click()
 
 time.sleep(5)
-from form import service
 driver.close()
